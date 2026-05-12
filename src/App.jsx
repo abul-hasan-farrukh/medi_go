0afec6adf69ae085b3dcae766fd1457b735cf3dc
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-05-12 21:34:07 +0530

    add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/common/Header"
+import Footer from "./components/common/Footer"
+import FetchFeedback from "./components/common/FetchFeedback"
+import PathologyChatbot from "./components/user/PathologyChatbotPage"
+import HomePage from "./components/common/HomePage"
+
+function App() {
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+export default App
