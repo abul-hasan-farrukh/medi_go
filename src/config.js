acd5ac9636e64a055b272ec7d09a44facd3beb9d
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-23 17:51:06 +0530

    add config file to store URLs

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:9090";
+const CHATBOT_URL = import.meta.env.VITE_CHATBOT_URL || "http://localhost:8080";
+
+export { BASE_URL, CHATBOT_URL };
