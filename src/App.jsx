89de110a8d04aeecc9576392b8c8d59e74bfb5ba
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-09-01 18:07:24 +0530

    fix: update import statement for Analytics component to use correct package

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/common/Footer"
 import FetchFeedback from "./components/common/FetchFeedback"
 import PathologyChatbot from "./components/user/PathologyChatbotPage"
 import HomePage from "./components/common/HomePage"
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
